f18a5684e9f3086c89e80e43775abd8ce60fc7e2
Gladymar O'Neill <gladymar@e-3consulting.com>
2020-06-08 07:26:42 -0400

    Buttons for more photos, experiences page with description options,flamboyan gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -236,5 +236,23 @@
         });
     }
 
+    var dataPopUp = $('.popup-with-zoom-anim');
+    if(dataPopUp.length) {
+        dataPopUp.magnificPopup({
+            type: 'inline',
+            fixedContentPos: false,
+            fixedBgPos: true,
+
+            overflowY: 'auto',
+
+            closeBtnInside: true,
+            preloader: false,
+
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-zoom-in'
+        })
+    }
+
 
 })(jQuery);
